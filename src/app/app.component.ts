bdabb7de00361f6df2e62bb1ac0d4182f5b874b4
KMatveychikov <matv00@gmail.com>
2022-09-19 19:10:09 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {products as data} from './data/products'
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
+import {ModalService} from "./services/modal.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+
+}
---
+++ src/app/pages/product-page/product-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductsService} from "../../services/products.service";
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.scss']
+})
+export class ProductPageComponent implements OnInit {
+  title = 'angular-minin';
+  // products: IProduct[] = []
+  loading = false
+  // $products: Observable<IProduct[]>
+  term = ''
+
+  constructor(
+    public productService: ProductsService,
+    public modalService: ModalService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    // this.$products = this.productService.getAll().pipe(
+    //   tap( () => this.loading = false)
+    // )
+
+    this.productService.getAll().subscribe(() => {
+      this.loading = false
+    })
+  }
+
+
+}
